6b44b6183035ec3363fdf1f454f8d1f7d455fb6b
charith <tilershub@gmail.com>
2025-08-14 15:18:16 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="vstack">
+          <div>© {new Date().getFullYear()} TILERSHUB</div>
+          <div>WhatsApp: 0774503744</div>
+        </div>
+      </div>
+    </footer>
+  )
+}
